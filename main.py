237d14247d831cd36e6829e0f857658dc8c4cbf9
Paulina Elizalde <pau87x@gmail.com>
2022-12-12 15:19:18 -0600

    Adding events

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ app = FastAPI(title='A little IMDB',
              description='With this project movies can be reviewed',
              version='1.0')
 
+@app.on_event('startup')
+def startup():
+    print('Starting server')
+
+@app.on_event('shutdown')
+def shutdown():
+    print('Shutdown server')
+
 @app.get('/')
 async def index():
     return 'hello world'
+
+@app.get('/about')
+async def about():
+    return 'about'
