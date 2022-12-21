df331992bdfec18c3f60c83588c96d955f10bd39
Paulina Elizalde <pau87x@gmail.com>
2022-12-21 16:54:50 -0600

    Changing app

---
+++ project/__init__.py
@@ -0,0 +1,99 @@
+from typing import List
+from fastapi import FastAPI, HTTPException
+from database import database as connection
+from database import User, Movie, UserReview
+from schemas import UserRequestModel, UserResponseModel
+from schemas import ReviewRequestModel, ReviewResponseModel, ReviewRequestPutModel
+from schemas import MovieRequestModel, MovieResponseModel
+
+app = FastAPI(title='A little IMDB',
+             description='With this project movies can be reviewed',
+             version='1.0')
+
+@app.on_event('startup')
+def startup():
+    if connection.is_closed():
+        connection.connect()
+        print('Starting server... connecting to db')
+
+    connection.create_tables([User, Movie, UserReview])
+
+@app.on_event('shutdown')
+def shutdown():
+    if not connection.is_closed():
+        connection.close
+        print('Shutdown server... closing connection')
+
+@app.post('/users',response_model=UserResponseModel)
+async def create_user(user: UserRequestModel):
+    if User.select().where(User.username == user.username).exists():
+        return HTTPException(409, 'El username se encuentra en uso')
+    hash_password = User.create_password(user.password)
+
+    user = User.create(
+        username = user.username, 
+        password = hash_password
+    )
+
+    return user
+
+@app.get('/reviews', response_model=List[ReviewResponseModel])
+async def get_reviews(page: int = 1, limit: int = 10):
+    reviews = UserReview.select().paginate(page,limit)
+
+    return [user_review for user_review in reviews]
+
+@app.get('/reviews/{review_id}', response_model=ReviewResponseModel)
+async def get_review(review_id: int):
+    review = UserReview.select().where(UserReview.id == review_id).first()
+
+    if review is None:
+        raise HTTPException(404, 'La review no fue encontrada')
+
+    return review
+
+@app.put('/reviews/{review_id}', response_model=ReviewResponseModel)
+async def update_review(review_id: int, review_request: ReviewRequestPutModel):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+
+    if user_review is None:
+        raise HTTPException(404, 'La review no fue encontrada')
+
+    user_review.review = review_request.review
+    user_review.score = review_request.score
+    user_review.save()
+
+    return user_review
+
+@app.post('/reviews',response_model=ReviewResponseModel)
+async def create_review(user_review: ReviewRequestModel):
+    if User.select().where(User.id == user_review.user_id).first() is None:
+        raise HTTPException(404, 'El usuario no fue encontrado')
+
+    if Movie.select().where(Movie.id == user_review.movie_id).first() is None:
+        raise HTTPException(404, 'La pelicula no fue encontrada')
+
+    user_review = UserReview.create(
+        user_id = user_review.user_id,
+        movie_id = user_review.movie_id,
+        review = user_review.review,
+        score = user_review.score
+        )
+    return user_review
+
+@app.delete('/reviews/{review_id}', response_model=ReviewResponseModel)
+async def delete_review(review_id: int):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+
+    if user_review is None:
+        raise HTTPException(404, 'La review no fue encontrada')
+
+    user_review.delete_instance()
+    return user_review
+
+@app.post('/movies',response_model=MovieResponseModel)
+async def create_movie(movie: MovieRequestModel):
+    movie = Movie.create(
+        title = movie.title
+        )
+    return movie
