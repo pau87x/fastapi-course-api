1a7878653916d9022aafa4b3c8924e2cdc4f5a73
Paulina Elizalde <pau87x@gmail.com>
2022-12-30 14:53:19 -0600

    Sending parameres with requests

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import requests
 
-URL = 'http://localhost:8000/api/v1/reviews?page=1&limit=2'
+URL = 'http://localhost:8000/api/v1/reviews'
 HEADERS = { 'accept': 'application/json' }
+QUERYSET= { 'page': 2, 'limit': 2}
 
-response = requests.get(URL, headers=HEADERS)
+response = requests.get(URL, headers=HEADERS, params=QUERYSET)
 
 if response.status_code == 200:
     print('Peticion exitosa')
