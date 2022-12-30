ddfed14e1125557b907bbfcb2ce8fe00b9e347c3
Paulina Elizalde <pau87x@gmail.com>
2022-12-30 14:15:46 -0600

    Add get reviews by user

--- project/routers/users.py
+++ project/routers/users.py
@@ -1,7 +1,8 @@
-from fastapi import HTTPException, APIRouter
+from typing import List
+from fastapi import HTTPException, APIRouter, Response, Cookie
 from fastapi.security import HTTPBasicCredentials
 from ..database import User
-from ..schemas import UserRequestModel, UserResponseModel
+from ..schemas import UserRequestModel, UserResponseModel, ReviewResponseModel
 
 router = APIRouter(prefix='/users')
 
@@ -19,13 +20,24 @@ async def create_user(user: UserRequestModel):
     return user
 
 @router.post('/login', response_model=UserResponseModel)
-async def login(credentials: HTTPBasicCredentials):
+async def login(credentials: HTTPBasicCredentials, response: Response):
     user = User.select().where(User.username == credentials.username).first()
 
     if user is None:
-        raise HTTPException(409, 'username no encontrado')
+        raise HTTPException(409, 'Usuario no encontrado')
 
     if user.password != User.create_password(credentials.password):
         raise HTTPException(404, 'Password error')
 
+    response.set_cookie(key='user_id', value=user.id)
     return user
+
+@router.get('/reviews', response_model=List[ReviewResponseModel])
+async def get_reviews(user_id: int = Cookie(None)):
+    user = User.select().where(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(409, 'Usuario no encontrado')
+
+    return [ user_review for user_review in user.reviews]
+
