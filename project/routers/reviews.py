f06c61eee42732967423f4cc003e50d18c1712f6
Paulina Elizalde <pau87x@gmail.com>
2023-01-07 16:52:11 -0600

    Refactor to update review

--- project/routers/reviews.py
+++ project/routers/reviews.py
@@ -39,11 +39,14 @@ async def get_review(review_id: int):
     return review
 
 @router.put('/{review_id}', response_model=ReviewResponseModel)
-async def update_review(review_id: int, review_request: ReviewRequestPutModel):
+async def update_review(review_id: int, review_request: ReviewRequestPutModel, user: User = Depends(get_current_user)):
     user_review = UserReview.select().where(UserReview.id == review_id).first()
 
     if user_review is None:
         raise HTTPException(404, 'La review no fue encontrada')
+
+    if user_review.user_id != user.id:
+        raise HTTPException(401, 'Usuario no autorizado')
 
     user_review.review = review_request.review
     user_review.score = review_request.score
